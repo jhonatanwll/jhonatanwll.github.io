abb2e651478cf58a70c90730db824e467c335f9d
Jhonatan <jhonatanwcom@gmail.com>
2024-04-15 15:27:30 -0300

    Comeco de filtro e css finalizado

--- todolistReact/todo/src/App.jsx
+++ todolistReact/todo/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Todo from "./components/Todo";
 import TodoForm from "./components/TodoForm";
 import Search from "./components/Search";
+import Filter from "./components/Filter";
 
 import "./App.css";
 
@@ -64,7 +65,7 @@ function App() {
     <div className="app">
       <h1>Lista de Tarefas</h1>
       <Search search={search} setSearch={setSearch} />
-
+      <Filter />
       <div className="todo-list">
         {todos
           .filter((todo) =>
