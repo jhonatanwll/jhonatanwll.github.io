c1e88f59368febee312f5b9071e1e88089b78586
Jhonatan <jhonatanwcom@gmail.com>
2024-04-15 15:19:33 -0300

    Funcionalidade de pesquisa implementada com React

--- todolistReact/todo/src/App.jsx
+++ todolistReact/todo/src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
+
 import Todo from "./components/Todo";
 import TodoForm from "./components/TodoForm";
+import Search from "./components/Search";
+
 import "./App.css";
 
 function App() {
@@ -24,6 +27,9 @@ function App() {
       isCompleted: false,
     },
   ]);
+
+  const [search, setSearch] = useState("");
+
   const addTodo = (text, category) => {
     const newTodos = [
       ...todos,
@@ -51,16 +57,27 @@ function App() {
     newTodos.map((todo) =>
       todo.id === id ? (todo.isCompleted = !todo.isCompleted) : todo
     );
-    setTodos(newTodos)
+    setTodos(newTodos);
   };
 
   return (
     <div className="app">
       <h1>Lista de Tarefas</h1>
+      <Search search={search} setSearch={setSearch} />
+
       <div className="todo-list">
-        {todos.map((todo) => (
-          <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo}/>
-        ))}
+        {todos
+          .filter((todo) =>
+            todo.text.toLowerCase().includes(search.toLowerCase())
+          )
+          .map((todo) => (
+            <Todo
+              key={todo.id}
+              todo={todo}
+              removeTodo={removeTodo}
+              completeTodo={completeTodo}
+            />
+          ))}
       </div>
       <TodoForm addTodo={addTodo} />
     </div>
