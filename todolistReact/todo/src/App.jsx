df2400d6908d9df981d5bc206de543bbc8dda6d2
Jhonatan <jhonatanwcom@gmail.com>
2024-04-18 15:13:21 -0300

    Implementada ordenação na Todo list com React. Todo list Finalizada

--- todolistReact/todo/src/App.jsx
+++ todolistReact/todo/src/App.jsx
@@ -31,6 +31,9 @@ function App() {
 
   const [search, setSearch] = useState("");
 
+  const [filter, setFilter] = useState("All");
+  const [sort, setSort] = useState("Asc");
+
   const addTodo = (text, category) => {
     const newTodos = [
       ...todos,
@@ -65,11 +68,23 @@ function App() {
     <div className="app">
       <h1>Lista de Tarefas</h1>
       <Search search={search} setSearch={setSearch} />
-      <Filter />
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort} />
       <div className="todo-list">
         {todos
           .filter((todo) =>
+            filter === "All"
+              ? true
+              : filter === "Completed"
+              ? todo.isCompleted
+              : !todo.isCompleted
+          )
+          .filter((todo) =>
             todo.text.toLowerCase().includes(search.toLowerCase())
+          )
+          .sort((a, b) =>
+            sort === "Asc"
+              ? a.text.localeCompare(b.text)
+              : b.text.localeCompare(a.text)
           )
           .map((todo) => (
             <Todo
--- todolistReact/todo/src/components/Filter.jsx
+++ todolistReact/todo/src/components/Filter.jsx
@@ -1,24 +1,24 @@
-const Filter = () => {
+const Filter = ({filter, setFilter, setSort}) => {
   return (
     <div className="filter">
-        <h2>Filtrar:</h2>
-        <div className="filter-options">
-            <div>
-                <p>Status:</p>
-                <select>
-                  <option value="All">Todas</option>  
-                  <option value="Completed">Completas</option>
-                  <option value="Incomplete">Incompletas</option>
-                </select>
-            </div>
-            <div>
-                <p>Ordem alfabética:</p>
-                <button>Asc</button>
-                <button>Desc</button>
-            </div>
+      <h2>Filtrar:</h2>
+      <div className="filter-options">
+        <div>
+          <p>Status:</p>
+          <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+            <option value="All">Todas</option>
+            <option value="Completed">Completas</option>
+            <option value="Incomplete">Incompletas</option>
+          </select>
         </div>
+        <div>
+          <p>Ordem alfabética:</p>
+          <button onClick={() => setSort("Asc")}>Asc</button>
+          <button onClick={() => setSort("Desc")}>Desc</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Filter
+export default Filter;
